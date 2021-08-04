30b2d02f947b8b7c90875c41927da95df26bc646
Seval Künler <knlr.swl@hotmail.com>
2021-08-04 15:46:21 +0300

    all complete

--- toDo.js
+++ toDo.js
@@ -1,7 +1,35 @@
-let addtoDoButton = document.getElementById("liveToastBtn");
-let list = document.getElementById("list")
-addtoDoButton.addEventListener("click", function(){
-    let li = document.createElement("li");
-    list.appendChild(li);
-    li.innerHTML= task.value;
-})
+const list = document.querySelector("ul#list");
+
+for (let i = 0; i < list.children.length; i++) {
+  addListEventListener(list.children[i]);
+}
+
+function addListEventListener(listElement) {
+  listElement.addEventListener("mousedown", function () {
+    this.classList.toggle("checked");
+  });
+
+  const closeSpan = document.createElement("span");
+  closeSpan.className = "close";
+  closeSpan.textContent = "x";
+
+  listElement.innerHTML += closeSpan.outerHTML;
+
+  listElement.children[0].addEventListener("mousedown", function () {
+    this.parentElement.remove();
+  });
+}
+
+function newElement() {
+  const task = document.getElementById("task");
+
+  if (task.value.trim()) {
+    const taskListItem = document.createElement("li");
+    taskListItem.textContent = task.value;
+
+    addListEventListener(taskListItem);
+    list.appendChild(taskListItem);
+
+   
+  }
+}
